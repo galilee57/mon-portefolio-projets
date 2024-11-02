69b107d1f06ff6a9f6f801e6ca8786f1cccf9912
galilee57 <fix.emmanuel@yahoo.fr>
2024-11-02 15:14:51 +0100

    Ajout de projets par thèmes

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,26 @@
-// src/Home/Home.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProjectCard from './components/ProjectCard';
+import projectsData from './projects.json';
 
 // Charger toutes les images du dossier assets/projects
 const images = require.context('./assets/projects', false, /\.(jpg|png|jpeg|gif)$/);
 
 const Home = () => {
-  const projects = [
-    {
-      title: "Projet Musculation",
-      description: "Une application pour suivre votre programme de musculation.",
-      image: images('./projectMusculation.jpg'), // correspond au nom du fichier dans le dossier
-      link: "/musculation",
-    },
-    {
-      title: "Projet Finance",
-      description: "Une application pour gérer vos finances de manière efficace.",
-      image: images('./projectFinance.jpg'),
-      link: "/finance",
-    },
-    {
-      title: "Projet JavaScript",
-      description: "Une collection de projets JavaScript pour améliorer vos compétences.",
-      image: images('./projectJS.jpg'),
-      link: "/projetsjs",
-    },
-  ];
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    // Ajouter les images correspondantes aux données JSON
+    const projectsWithImages = projectsData.map(project => ({
+      ...project,
+      image: images(`./${project.image}`)
+    }));
+
+    // Trier les projets par ordre alphabétique de 'theme'
+    projectsWithImages.sort((a, b) => a.theme.localeCompare(b.theme));
+
+    // Mettre à jour le state avec les projets triés
+    setProjects(projectsWithImages);
+  }, []);
 
   return (
     <div className="home-page">
@@ -34,6 +29,7 @@ const Home = () => {
         {projects.map((project, index) => (
           <ProjectCard
             key={index}
+            theme={project.theme}
             title={project.title}
             description={project.description}
             image={project.image}
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 import './ProjectCard.css';
 
-const ProjectCard = ({ title, description, image, link }) => {
+const ProjectCard = ({ theme, title, description, image, link }) => {
   return (
     <div className="project-card">
       <img src={image} alt={title} className="project-card-image" />
       <div className="project-card-content">
+        <h2 className="project-card-thema">{theme}</h2>
         <h2 className="project-card-title">{title}</h2>
         <p className="project-card-description">{description}</p>
         {/* Utilisez Link pour une navigation côté client */}
